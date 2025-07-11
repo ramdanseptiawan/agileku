e68518b7e69a1baec161f6c609289d5823f6fe3b
Ramdan Septiawan <ramdan.einstein@gmail.com>
2025-07-11 19:02:06 +0700

    siap build v2

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/tjphack/new/lms/agileku/src/app/page.js
+// File: C:\Users\ramda\Documents\project\lms\agileku\src\app\page.js
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
